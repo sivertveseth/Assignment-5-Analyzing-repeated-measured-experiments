f5cc5d2540c91a4087bf161fda507da8a2bbdbf5
Fittnix <sivertveseth@gmail.com>
2024-11-07 23:40:39 +0100

    Update R.R

--- R.R
+++ R.R
@@ -115,3 +115,154 @@ ggplot(data, aes(x = factor(Uke), y = Frekvens)) +
 kable(baseline_tbl, row.names = FALSE, col.names = c("", "Ekskludert", "Inkludert", "Ekskludert", "Inkludert")) %>% 
   kable_styling(bootstrap_options = c("striped", "hover", "condensed"), full_width = FALSE) %>%
   add_header_above(c(" " = 1, "Kvinne" = 2, "Mann" = 2))
+
+
+# Utregning av gjennomsnitt og SD for muskeltverrsnitt
+library(dplyr)
+library(knitr)
+library(ggplot2)
+
+# Dataprosessering - omstrukturering og kombinering av data for høyre og venstre ben
+muskeltverr <- dxadata %>%
+  select(participant:include, lean.left_leg, lean.right_leg) %>%
+  pivot_longer(
+    cols = lean.left_leg:lean.right_leg, 
+    names_to = "leg", 
+    values_to = "lean.mass"
+  ) %>%
+  mutate(
+    leg = if_else(leg == "lean.left_leg", "L", "R"), 
+    sets = if_else(multiple == leg, "multiple", "single")
+  ) %>%
+  select(participant, time, sex, include, sets, leg, lean.mass)
+
+# Oppsummering av gjennomsnitt og standardavvik etter tid, treningsvolum og kjønn
+muskeltverr_summary <- muskeltverr %>% 
+  group_by(time, sets, sex) %>% 
+  summarise(
+    mean_lean_mass = mean(lean.mass, na.rm = TRUE),
+    sd_lean_mass = sd(lean.mass, na.rm = TRUE),
+    .groups = "drop"  # Fjerner grupperingen automatisk etter summarise
+  )
+
+# Filtrer til pre og post tidspunkter og kombiner kjønn for en enklere oversikt
+muskeltverr_summary_pre_post <- muskeltverr_summary %>%
+  filter(time %in% c("pre", "post")) %>%
+  group_by(time, sets) %>%
+  summarise(
+    mean_lean_mass = mean(mean_lean_mass, na.rm = TRUE),
+    sd_lean_mass = sqrt(mean(sd_lean_mass^2, na.rm = TRUE)),  # Kombinert SD ved å bruke kvadratet av SD
+    .groups = "drop"
+  ) %>%
+  arrange(time, sets)  # Sortér etter tid og treningsvolum
+
+# Lag en oversiktlig tabell
+muskeltverr_summary_pre_post %>%
+  kable(
+    col.names = c("Tidspunkt", "Treningsvolum", "Gjennomsnittlig muskeltverrsnitt (g)", "Standardavvik (SD)"),
+    caption = "Oppsummering av muskeltverrsnitt før og etter intervensjon (kombinert for kjønn, kun pre og post)"
+  )
+
+# Utregning av gjennomsnitt og SD for muskelstyrke
+
+library(dplyr)
+
+strength_summary <- strengthvolume %>% 
+  filter(exercise %in% c("legpress", "legext")) %>% 
+  group_by(time, sets, sex, exercise) %>% 
+  summarise(mean_strength = mean(load, na.rm = TRUE),
+            sd_strength = sd(load, na.rm = TRUE)) %>% 
+  ungroup()
+
+# Diagram av utviklingen i muskelstyrke
+
+library(ggplot2)
+
+## Endre navn på tidspunktene
+
+strength <- strength_mean.sd %>%
+  mutate(time = recode(time,
+                       "pre" = "Pre",
+                       "session1" = "Uke 1",
+                       "week2" = "Uke 2",
+                       "week5" = "Uke 5",
+                       "week9" = "Uke 9",
+                       "post" = "Post"),
+         time = factor(time, levels = c("Pre", "Uke 1", "Uke 2", "Uke 5", "Uke 9", "Post")))
+
+ggplot(strength, aes(x = time, y = mean_strength, color = sets, shape = sex, group = interaction(sets, sex, exercise))) +
+  geom_line(size = 1) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_strength - sd_strength, ymax = mean_strength + sd_strength), width = 0.2) +
+  facet_wrap(~ exercise) +
+  labs(x = "Tidspunkt", y = "Gjennomsnittlig muskelstyrke (kg)", color = "Treningsvolum", shape = "Kj\u00F8nn",
+       title = "Utvikling i muskelstyrke for leg press og leg extension") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
+
+library(nlme)
+
+# Modell for muskelstyrke i legpress øvelsen uten manglende verdier
+model_legpress <- lme(
+  fixed = load ~ time * sets + sex,
+  random = ~ 1 | participant,
+  data = strengthvolume %>% filter(exercise == "legpress"),
+  na.action = na.omit
+)
+
+summary(model_legpress)
+
+# Ekstraher residualene fra modellen
+residuals_legpress <- residuals(model_legpress)
+
+# Ekstraher predikerte verdier fra modellen
+fitted_values_legpress <- fitted(model_legpress)
+
+# Q-Q plot
+qqnorm(residuals_legpress)
+qqline(residuals_legpress, col = "red")
+
+library(MASS)
+
+# Bruk Box-Cox transformasjon for å finne optimal lambda-verdi
+boxcox_model <- boxcox(lm(load ~ time * sets + sex, data = strengthvolume))
+lambda <- boxcox_model$x[which.max(boxcox_model$y)]
+
+# Transformér dataene med optimal lambda
+strengthvolume <- strengthvolume %>%
+  mutate(boxcox_load = (load^lambda - 1) / lambda)
+
+# Log-transformasjon av variabelen `load` basert på Box Cox plottet
+strengthvolume <- strengthvolume %>%
+  mutate(log_load = log(load))
+
+# Endring av modellen
+model_log_legpress <- lme(
+  fixed = log_load ~ time * sets + sex,
+  random = ~ 1 | participant,
+  data = strengthvolume %>% filter(exercise == "legpress"),
+  na.action = na.omit
+)
+
+# Ekstraher residualene fra den nye modellen
+residuals_legpress_log <- residuals(model_log_legpress)
+
+# Ekstraher predikerte verdier fra den nye modellen
+fitted_values_legpress_log <- fitted(model_log_legpress)
+
+# Q-Q plot av nye modellen
+qqnorm(residuals_legpress_log)
+qqline(residuals_legpress_log, col = "red")
+
+# Plot residualene mot de predikerte verdiene
+ggplot(data = NULL, aes(x = fitted_values_legpress_log, y = residuals_legpress_log)) +
+  geom_point() +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
+  labs(x = "Predikerte verdier", y = "Residualer", title = "Residualer vs. Predikerte Verdier") +
+  theme_minimal()
+
+# Lag et histogram for residualene
+ggplot(data = NULL, aes(x = residuals_legpress_log)) +
+  geom_histogram(binwidth = 5, fill = "steelblue", color = "black", alpha = 0.7) +
+  labs(x = "Residualer", y = "Frekvens", title = "Histogram av Residualer") +
+  theme_minimal()
